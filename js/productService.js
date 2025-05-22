eda0ad0b5bbb9286db51b5253cf3263e8c327669
cristiancrakl <moralescadenac@gmail.com>
2025-05-21 20:42:54 -0500

    haciendo cambios mas grandes a shop

--- js/productService.js
+++ js/productService.js
@@ -1,26 +1,24 @@
-function getProducts(){
+function getProducts() {
     document.getElementById('cardHeader').innerHTML = '<h4>Listado de productos</h4>'
     document.getElementById('info').innerHTML = ''
-    fetch("https://reqres.in/api/unknown", {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json",
-            'x-api-key': 'reqres-free-v1'
-        },
+    fetch('https://fakestoreapi.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(credentials)
     })
-    .then((result) =>{
-        return result.json().then(
-            data => {
-                return {
-                    status: result.status,
-                    body: data
+        .then((result) => {
+            return result.json().then(
+                data => {
+                    return {
+                        status: result.status,
+                        body: data
+                    }
                 }
-            }
-        )
-    })
-    .then((response) =>{
-        if(response.status === 200){
-            let listproducts = `
+            )
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                let listproducts = `
                 <table class="table table-striped table-bordered border-dark">
                 <thead>
                     <tr >
@@ -33,25 +31,25 @@ function getProducts(){
                 </thead>
                 <tbody class="table-group-divider">
             `
-            response.body.data.forEach(user => {
-                listproducts = listproducts.concat(`
+                response.body.data.forEach(user => {
+                    listproducts = listproducts.concat(`
                     <tr>
                         <td>${user.id}</td>
-                        <td>${user.name}</td>
-                        <td>${user.year}</td>
+                        <td>${user.title}</td>
+                        <td>${user.slug}</td>
                         <td><input type="color" value="${user.color}"></td>
                         <td>${user.pantone_value}</td>
                     </tr>
                     `)
-            });
-            listproducts = listproducts.concat(`
+                });
+                listproducts = listproducts.concat(`
                 </tbody>
             </table>
             `)
-            document.getElementById('info').innerHTML = listproducts
-        }
-        else{
-            document.getElementById('info').innerHTML = '<h3>No se encontraron productos</h3>'
-        }
-    })
+                document.getElementById('info').innerHTML = listproducts
+            }
+            else {
+                document.getElementById('info').innerHTML = '<h3>No se encontraron productos</h3>'
+            }
+        })
 }
